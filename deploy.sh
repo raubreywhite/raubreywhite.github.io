1c7e829a4c6d04d10847b7c12a1fed3c1e6653c0
Richard Aubrey White <RichardAubrey.White@fhi.no>
2022-10-11 08:16:43 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 reset
 Rscript -e "quarto::quarto_render('.')"
-cp -r articles favicon.png _site/
+cp -r articles favicon.png CNAME _site/
 git add _site && git commit -m "Initial _site subtree commit"
 git push origin `git subtree split --prefix _site main`:gh-pages --force
 git reset --soft HEAD^
