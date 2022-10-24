c5e02e888385a759a706e726f86031430c6e8280
Richard Aubrey White <hello@rwhite.no>
2022-10-24 10:17:16 +0200

    x

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,3 @@ sleep 5
 git add docs && git commit -m "docs commit"
 sleep 5
 git push origin main:main
-git subtree split --branch gh-pages --prefix _site
-git push origin gh-pages:gh-pages
-git push origin `git subtree split --prefix _site main`:gh-pages --force
-sleep 5
-git reset --soft HEAD^
-git restore --staged .
-rm -rf _site
