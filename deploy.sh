dfd38059d0a75c7843fbe2a6b491ddc6cc3c0cc1
Richard Aubrey White <hello@rwhite.no>
2022-10-24 06:35:35 +0200

    cleaning up

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,14 @@
 
 reset
 Rscript -e "quarto::quarto_render('.')"
-cp -r articles favicon.png CNAME _site/
-cp -r resources/* _site/resources
+cp -r articles favicon.png CNAME docs/
+cp -r resources/* docs/resources
 sleep 5
-git add _site && git commit -m "Initial _site subtree commit"
+git add docs && git commit -m "docs commit"
 sleep 5
+git push origin main:main
 git subtree split --branch gh-pages --prefix _site
+git push origin gh-pages:gh-pages
 git push origin `git subtree split --prefix _site main`:gh-pages --force
 sleep 5
 git reset --soft HEAD^
