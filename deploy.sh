3aee2ff55a7e50762c13aae91ed5063bdd060110
Richard Aubrey White <hello@rwhite.no>
2022-10-24 06:23:17 +0200

    x

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ cp -r resources/* _site/resources
 sleep 5
 git add _site && git commit -m "Initial _site subtree commit"
 sleep 5
+git subtree split --branch gh-pages --prefix _site
 git push origin `git subtree split --prefix _site main`:gh-pages --force
 sleep 5
 git reset --soft HEAD^
