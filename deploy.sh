33034b0166c994a5844e511ae6f75d52bbd70469
Richard Aubrey White <hello@rwhite.no>
2023-01-26 05:45:45 +0100

    Push

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ reset
 Rscript -e "quarto::quarto_render('.')"
 cp -r articles favicon.png CNAME docs/
 cp -r resources/* docs/resources
-sleep 5
 git add docs && git commit -m "docs commit"
-sleep 5
 git push origin main:main
+
